18b304ca84ee3d4d0e18f4729bf65c08718ce18f
Julien Gabryelewicz <julien.gabryelewicz2@gmail.com>
2022-07-27 09:32:18 +0200

    Fix bugs

--- student/src/database.sql
+++ student/src/database.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE application_gestion_classe CHARACTER SET 'utf8';
 
 CREATE TABLE classes (
-    id int UNSIGNED AUTO_INCREMENT,
+    id int UNSIGNED AUTO_INCREMENT NOT NULL,
     nom varchar(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE matieres (
-    id int UNSIGNED AUTO_INCREMENT,
+    id int UNSIGNED AUTO_INCREMENT NOT NULL,
     nom varchar(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE eleves (
-    id INT UNSIGNED AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     prenom VARCHAR(100) NOT NULL,
     nom VARCHAR(100) NOT NULL,
-    date_naissance DATE,
+    date_naissance DATE NOT NULL,
     commentaire TEXT,
     id_classe INT UNSIGNED,
     PRIMARY KEY (id),
@@ -25,11 +25,11 @@ CREATE TABLE eleves (
 
 
 CREATE TABLE notes (
-    id INT UNSIGNED AUTO_INCREMENT,
-    nom VARCHAR(255),
-    note FLOAT(4,2),
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    note FLOAT(4,2) NOT NULL,
     commentaire TEXT,
-    date_note DATE,
+    date_note DATE NOT NULL,
     id_eleve INT UNSIGNED,
     id_matiere INT UNSIGNED,
     PRIMARY KEY (id),
